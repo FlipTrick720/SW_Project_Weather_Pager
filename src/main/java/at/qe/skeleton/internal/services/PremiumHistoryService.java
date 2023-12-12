a8a7aac8313f480e0c99f5b6f6f7869aac501194
Alex Hemmen <git config --global user.email alex.hemmen@student.uibk.ac.at>
2023-12-12 17:35:19 +0100

    Table PremiumHistory for H2. Every change of the premium Status evolves in a entry in the database. added the missing class.

---
+++ src/main/java/at/qe/skeleton/internal/services/PremiumHistoryService.java
@@ -0,0 +1,37 @@
+package at.qe.skeleton.internal.services;
+
+import at.qe.skeleton.internal.model.PremiumHistory;
+import at.qe.skeleton.internal.model.Userx;
+import at.qe.skeleton.internal.repositories.PremiumHistoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ * Service for accessing and  manipulating Premium History data.
+ */
+@Component
+@Scope("application")
+public class PremiumHistoryService {
+
+    @Autowired
+    private PremiumHistoryRepository premiumHistoryRepository;
+
+    public void savePremiumHistory(Userx user, boolean newPremiumStatus) {
+        PremiumHistory premiumHistory = new PremiumHistory();
+        premiumHistory.setUser(user);
+        premiumHistory.setChangeDate(LocalDateTime.now());
+        premiumHistory.setNewPremiumStatus(newPremiumStatus);
+        premiumHistoryRepository.save(premiumHistory);
+    }
+    @PreAuthorize("hasAuthority('MANAGER')" )
+    public List<PremiumHistory> getPremiumChanged(String user) {
+        return premiumHistoryRepository.findAllByUserId(user);
+    }
+
+
+}
