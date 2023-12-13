af4679dab7c6406083027caec500aa357fb4a610
Malte Braig <csba5721@student.uibk.ac.at>
2023-12-13 13:31:32 +0100

    selbe ausgabe aber mit prototyp der Premium intervall ausgabe

--- src/main/java/at/qe/skeleton/internal/ui/controllers/PremiumStatusListener.java
+++ src/main/java/at/qe/skeleton/internal/ui/controllers/PremiumStatusListener.java
@@ -4,11 +4,15 @@ package at.qe.skeleton.internal.ui.controllers;
 import at.qe.skeleton.internal.model.PremiumHistory;
 import at.qe.skeleton.internal.model.Userx;
 import at.qe.skeleton.internal.services.PremiumHistoryService;
+import jakarta.validation.constraints.Null;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -31,27 +35,39 @@ public class PremiumStatusListener implements PropertyChangeListener{
 
     public List<PremiumHistory> getPremiumIntervalByName(Userx user) {
         return premiumHistoryService.getPremiumChangedByName(user.getUsername());
-        //tupel von aktiven premium phasen
     }
 
     public List<PremiumHistory> getPremiumInterval() {
         return premiumHistoryService.getPremiumChanged();
     }
 
-    /*
     //work in progress
-    public List<PremiumHistory> getPremiumTupel(Userx user) {
+    public List<Integer> getPremiumTupel(Userx user) {
         List<PremiumHistory> allDates = getPremiumIntervalByName(user);
-        Map<Userx, List<PremiumHistory>> DatesByUser = allDates.stream().collect(Collectors.groupingBy(PremiumHistory::getUser));
-
-
-
-
-        for (int i = 0; i < allDates.toArray().length-1; i++) {
-            if (allDates.get(i).getUser().equals(allDates.get(i+1).getUser())) {
-                allDates.get(i)
+        List<Duration> intervalls = new ArrayList<>();
+        if (allDates.toArray().length < 2) {
+            return null;
+        } else if (allDates.toArray().length%2 == 0) {
+            for (int i = 0; i < allDates.toArray().length-1; i=i+2) {
+                intervalls.add(Duration.between(allDates.get(i).getChangeDate(), allDates.get(i+1).getChangeDate()));
+            }
+        } else {
+            for (int i = 0; i < allDates.toArray().length-2; i=i+2) {
+                intervalls.add(Duration.between(allDates.get(i).getChangeDate(), allDates.get(i+1).getChangeDate()));
             }
         }
-    }*/
+        List<Integer> intervallsInInt = new ArrayList<>();
+        for (Duration duration : intervalls) {
+            intervallsInInt.add((int) duration.toSeconds());
+        }
+        return intervallsInInt;
+    }
 }
+
+/*
+        List<PremiumHistory> allDates = getPremiumIntervalByName(user);
+        Map<Userx, List<PremiumHistory>> DatesByUser = allDates.stream().collect(Collectors.groupingBy(PremiumHistory::getUser));
+        System.out.println(DatesByUser);
+        return DatesByUser.get(allDates.get(1).getUser());
+        * */
 
