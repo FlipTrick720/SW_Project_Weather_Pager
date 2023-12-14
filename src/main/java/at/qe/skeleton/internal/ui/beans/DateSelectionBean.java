0c6e76062bb97d9a4eb4284314c647445635a89a
Alex Hemmen <git config --global user.email alex.hemmen@student.uibk.ac.at>
2023-12-14 16:04:49 +0100

    DropDown Menues to choose Year and Month of manager View. (NO functionality implemented)

---
+++ src/main/java/at/qe/skeleton/internal/ui/beans/DateSelectionBean.java
@@ -0,0 +1,68 @@
+package at.qe.skeleton.internal.ui.beans;
+
+
+import jakarta.annotation.ManagedBean;
+import jakarta.annotation.PostConstruct;
+import jakarta.faces.annotation.ManagedProperty;
+import jakarta.faces.view.ViewScoped;
+
+import java.io.Serializable;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.List;
+
+@ManagedBean
+@ViewScoped
+
+public class DateSelectionBean implements Serializable {
+
+    private List<Integer> years;
+    private List<Month> months;
+    private Integer selectedYear;
+    private Month selectedMonth;
+
+    public DateSelectionBean() {
+        years = new ArrayList<>();
+        int currentYear = Calendar.getInstance().get(Calendar.YEAR);
+        for (int i = currentYear; i >= currentYear - 10; i--) {
+            years.add(i);
+        }
+
+
+        months = Arrays.asList(Month.JANUARY, Month.FEBRUARY, Month.MARCH, Month.APRIL, Month.MAY,
+                Month.JUNE, Month.JULY, Month.AUGUST, Month.SEPTEMBER, Month.OCTOBER, Month.NOVEMBER, Month.DECEMBER);
+    }
+
+    public List<Integer> getYears() {
+        return years;
+    }
+
+    public Integer getSelectedYear() {
+        return selectedYear;
+    }
+
+    public void setSelectedYear(Integer selectedYear) {
+        this.selectedYear = selectedYear;
+    }
+
+    public List<Month> getMonths() {
+        return months;
+    }
+
+    public Month getSelectedMonth() {
+        return selectedMonth;
+    }
+
+    public void setSelectedMonth(Month selectedMonth) {
+        this.selectedMonth = selectedMonth;
+    }
+
+    public String yourMethod() {
+        System.out.println("Selected Year: " + selectedYear);
+        System.out.println("Selected Month: " + selectedMonth);
+
+        return "/manager/user_list.xhtml";
+    }
+}
