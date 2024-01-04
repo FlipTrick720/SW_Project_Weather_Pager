076cd81e35ca8f3a71b11f0c29c4de9877e15e07
csba5755 <jakob.osl@student.uibk.ac.at>
2024-01-04 19:01:09 +0100

    landing page: favorite button can only be used when logged in, 24h details for normal users, 28h details for premiums

---
+++ src/main/java/at/qe/skeleton/internal/ui/beans/HourlyWeatherBean.java
@@ -0,0 +1,30 @@
+package at.qe.skeleton.internal.ui.beans;
+
+import at.qe.skeleton.external.model.currentandforecast.misc.HourlyWeatherDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@Scope("view")
+public class HourlyWeatherBean {
+    @Autowired
+    SessionInfoBean sessionInfoBean;
+
+    @Autowired
+    WeatherBean weatherBean;
+
+    public String getTitle() {
+        return sessionInfoBean.isPremium() ? "Next 48h Details" : "Next 24h Details";
+    }
+
+    public List<HourlyWeatherDTO> getHourlyWeather() {
+        if (sessionInfoBean.isPremium()){
+            return weatherBean.getWeather().hourlyWeather().subList(0, 48);
+        } else {
+            return weatherBean.getWeather().hourlyWeather().subList(0, 24);
+        }
+    }
+}
