9d07a8fcabf78ad9732ed5171d9faa7d90739b19
Malte Braig <csba5721@student.uibk.ac.at>
2024-01-07 13:13:13 +0100

    sonarqube test

--- src/test/java/at/qe/skeleton/tests/PaymentHistoryTest.java
+++ src/test/java/at/qe/skeleton/tests/PaymentHistoryTest.java
@@ -114,9 +114,7 @@ public class PaymentHistoryTest {
         Duration duration = Duration.between(now, lastDayOfMonth);
 
         long totaldifferenz = duration.toSeconds() - paymentList.get(0).getChargedDays();
-        System.out.println(totaldifferenz);
-        Assertions.assertTrue(totaldifferenz <= 1, "totaldifferenz should be less than 1");
-        Assertions.assertEquals(0, totaldifferenz);
+        Assertions.assertTrue(totaldifferenz <= 1, "totaldifferenz should be less than 1 second");
     }
 
     /*@Test
