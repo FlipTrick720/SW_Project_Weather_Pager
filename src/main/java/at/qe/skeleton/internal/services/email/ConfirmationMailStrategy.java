0404bf82e570fdb68cbc500d4fbd4246331396b6
csba5755 <jakob.osl@student.uibk.ac.at>
2024-01-13 17:09:36 +0100

    javadoc email

--- src/main/java/at/qe/skeleton/internal/services/email/ConfirmationMailStrategy.java
+++ src/main/java/at/qe/skeleton/internal/services/email/ConfirmationMailStrategy.java
@@ -2,6 +2,9 @@ package at.qe.skeleton.internal.services.email;
 
 import org.springframework.mail.SimpleMailMessage;
 
+/**
+ * Implementation of {@link EmailStrategy} for configuring confirmation email messages.
+ */
 public class ConfirmationMailStrategy implements EmailStrategy {
     @Override
     public void configureMail(SimpleMailMessage message, String token) {
