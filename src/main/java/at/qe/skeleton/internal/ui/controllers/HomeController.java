859882d4cbb030e3fe568aa73c2d03cef03c8059
csba5755 <jakob.osl@student.uibk.ac.at>
2024-01-22 13:15:39 +0100

    smell: simpler annotation

--- src/main/java/at/qe/skeleton/internal/ui/controllers/HomeController.java
+++ src/main/java/at/qe/skeleton/internal/ui/controllers/HomeController.java
@@ -2,8 +2,6 @@ package at.qe.skeleton.internal.ui.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Controller for setting the home page.
